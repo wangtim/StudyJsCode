c82d9063f74dcd3ceb8c16a910d8fdfdd1d1f2ad
wangtim <wang_tim@163.com>
2013-02-28 01:09:01 +0800

    Source first commit

---
+++ Java/helloworld.js
@@ -0,0 +1,18 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: WTM
+ * Date: 13-2-15
+ * Time: 下午10:31
+ * To change this template use File | Settings | File Templates.
+ * 函数名：hello
+ * hello函数将打印"Hello,World by js File"
+ */
+
+function hello()
+{
+    //向世界问好
+    var msg = "Hello,<em>World</em> by JavaScript File";
+    document.open();
+    document.write(msg);
+    document.close();
+}
---
+++ Java/test1.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: WTM
+ * Date: 13-1-16
+ * Time: 上午12:06
+ * To change this template use File | Settings | File Templates.
+ */
